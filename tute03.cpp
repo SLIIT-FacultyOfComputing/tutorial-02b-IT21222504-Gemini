e05c596adc981fb5430d3253dc1b3b5f5f74c64c
IT21222504-Gemi <99594916+IT21222504-Gemini@users.noreply.github.com>
2022-03-05 13:42:51 +0000

    completed

--- tute03.cpp
+++ tute03.cpp
@@ -1,9 +1,3 @@
-/*Exercise 3 - Repeatition
-
-Convert the C program given below which calculates the Factorial of a number that you input from the keyboard to a C++ program.
-
-Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
-
 #include <stdio.h>
 int main()
 {
--- tute04.cpp
+++ tute04.cpp
@@ -1,16 +1,3 @@
-/*Exercise 4 - Functions
-
-Write a program to calculate the function called nCr which is defined as
-
-nCr = n!/ r!(n−r)!
-Where n! is the factorial of n.
-
-Implement the functions
-
-long Factorial(int no);
-long nCr(int n, int r);
-Do not modify the main function.*/
-
 #include <iostream>
 
 long Factorial(int no);
